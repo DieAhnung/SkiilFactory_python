5ab7d8c902edbc23b31954de58d7c2f517a8eb5f
DieAhnung <76071438+DieAhnung@users.noreply.github.com>
2021-03-22 00:00:15 +0300

    ЗАДАНИЕ 12.7.3

---
+++ venv/Home_work_12/12_7.py
@@ -0,0 +1,12 @@
+money = int(input("Введите сумму: "))
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit = per_cent.items()
+
+for key, value in per_cent.items():
+
+    deposit = round((value * money) / 100)
+
+    print(deposit)
+
+# print("Максимальная сумма, которую вы можете заработать" + m_dep )
+
